04c605055baae0553577124c1523a780f383d021
Omar Adel <omaradel1.dev@gmail.com>
2026-01-13 07:22:46 +0200

    feat(statistics): implement statistics data models
    - Create StatisticsData model to represent monthly financial summaries.
    - Add CategoryBreakdown model for per-category spending/earning details.
    - Implement helper methods for percentage and total calculations.

---
+++ lib/statistics/data/models/statistics_model.dart
@@ -0,0 +1,113 @@
+import 'dart:convert';
+import 'package:equatable/equatable.dart';
+import 'package:budget_wise/home/data/models/home_model.dart';
+
+enum StatisticsSorting {
+  highestAmount,
+  lowestAmount,
+  alphabetical,
+}
+
+class StatisticsModel extends Equatable {
+  final double totalIncome;
+  final double totalExpenses;
+  final double totalSavings;
+  final List<CategoriesWithSpending> incomeBreakdown;
+  final List<CategoriesWithSpending> expenseBreakdown;
+  final List<double> dailyIncomeTrend;
+  final List<double> dailyExpenseTrend;
+  final StatisticsSorting sortingType;
+  final DateTime selectedMonth;
+
+  const StatisticsModel({
+    required this.totalIncome,
+    required this.totalExpenses,
+    required this.totalSavings,
+    required this.incomeBreakdown,
+    required this.expenseBreakdown,
+    required this.dailyIncomeTrend,
+    required this.dailyExpenseTrend,
+    required this.sortingType,
+    required this.selectedMonth,
+  });
+
+  @override
+  List<Object?> get props => [
+        totalIncome,
+        totalExpenses,
+        totalSavings,
+        incomeBreakdown,
+        expenseBreakdown,
+        dailyIncomeTrend,
+        dailyExpenseTrend,
+        sortingType,
+        selectedMonth,
+      ];
+
+  StatisticsModel copyWith({
+    double? totalIncome,
+    double? totalExpenses,
+    double? totalSavings,
+    List<CategoriesWithSpending>? incomeBreakdown,
+    List<CategoriesWithSpending>? expenseBreakdown,
+    List<double>? dailyIncomeTrend,
+    List<double>? dailyExpenseTrend,
+    StatisticsSorting? sortingType,
+    DateTime? selectedMonth,
+  }) {
+    return StatisticsModel(
+      totalIncome: totalIncome ?? this.totalIncome,
+      totalExpenses: totalExpenses ?? this.totalExpenses,
+      totalSavings: totalSavings ?? this.totalSavings,
+      incomeBreakdown: incomeBreakdown ?? this.incomeBreakdown,
+      expenseBreakdown: expenseBreakdown ?? this.expenseBreakdown,
+      dailyIncomeTrend: dailyIncomeTrend ?? this.dailyIncomeTrend,
+      dailyExpenseTrend: dailyExpenseTrend ?? this.dailyExpenseTrend,
+      sortingType: sortingType ?? this.sortingType,
+      selectedMonth: selectedMonth ?? this.selectedMonth,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'totalIncome': totalIncome,
+      'totalExpenses': totalExpenses,
+      'totalSavings': totalSavings,
+      'incomeBreakdown': incomeBreakdown.map((x) => x.toMap()).toList(),
+      'expenseBreakdown': expenseBreakdown.map((x) => x.toMap()).toList(),
+      'dailyIncomeTrend': dailyIncomeTrend,
+      'dailyExpenseTrend': dailyExpenseTrend,
+      'sortingType': sortingType.index,
+      'selectedMonth': selectedMonth.millisecondsSinceEpoch,
+    };
+  }
+
+  factory StatisticsModel.fromMap(Map<String, dynamic> map) {
+    return StatisticsModel(
+      totalIncome: (map['totalIncome'] as num).toDouble(),
+      totalExpenses: (map['totalExpenses'] as num).toDouble(),
+      totalSavings: (map['totalSavings'] as num).toDouble(),
+      incomeBreakdown: List<CategoriesWithSpending>.from(
+        (map['incomeBreakdown'] as List<dynamic>).map<CategoriesWithSpending>(
+          (x) => CategoriesWithSpending.fromMap(x as Map<String, dynamic>),
+        ),
+      ),
+      expenseBreakdown: List<CategoriesWithSpending>.from(
+        (map['expenseBreakdown'] as List<dynamic>).map<CategoriesWithSpending>(
+          (x) => CategoriesWithSpending.fromMap(x as Map<String, dynamic>),
+        ),
+      ),
+      dailyIncomeTrend: List<double>.from(map['dailyIncomeTrend'] as List),
+      dailyExpenseTrend: List<double>.from(map['dailyExpenseTrend'] as List),
+      sortingType: StatisticsSorting.values[map['sortingType'] as int],
+      selectedMonth: DateTime.fromMillisecondsSinceEpoch(
+        map['selectedMonth'] as int,
+      ),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory StatisticsModel.fromJson(String source) =>
+      StatisticsModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}
